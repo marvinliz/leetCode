7550a14e33177c26837b2c4d5ceb9e2ce54f12b8
LiDaQian <195853932@qq.com>
2019-04-09 19:36:19 +0800

    Patch

--- src/main/java/greedy/BoatsToSavePeople.java
+++ src/main/java/greedy/BoatsToSavePeople.java
@@ -16,16 +16,16 @@ public class BoatsToSavePeople {
         }
 
         Arrays.sort(people);
-
+        // 排序后，挑选最大和最小的人上船
+        // 如果最大+最小比 limit 大，则选择最大的上船
+        // 否则最大和最小上船
         int result = 0;
-        for (int i = 0, j = people.length - 1; i <= j;) {
-            if (people[i] + people[j] <= limit) {
+        for (int low = 0, high = people.length - 1; low <= high; high--) {
+            if (people[low] + people[high] <= limit) {
                 result++;
-                i++;
-                j--;
+                low++;
             } else {
                 result++;
-                j--;
             }
         }
 
